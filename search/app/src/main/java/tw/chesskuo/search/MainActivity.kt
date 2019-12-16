16e9c2df37f550325de7ef3159dc6b510d943247
Chess Kuo <gyc880323@gmail.com>
2019-12-16 17:21:56 +0800

    code clean ok

--- search/app/src/main/java/tw/chesskuo/search/MainActivity.kt
+++ search/app/src/main/java/tw/chesskuo/search/MainActivity.kt
@@ -12,51 +12,58 @@ import java.text.*
 import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+const val proto = "https://"
+var region = ""
+const val host = ".api.riotgames.com"
+val mode = mapOf("summoner" to "/lol/summoner/v4/summoners/by-name/",
+                "league" to "/lol/league/v4/entries/by-summoner/",
+                "match" to "/lol/match/v4/matchlists/by-account/"
+            )
+const val api_key = "?api_key=" + "RGAPI-9d877f40-5f36-4ce3-b3bb-f0830e54977d"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+var id = ""
+var accountId = ""
+
+
+
+
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val spinner = findViewById(R.id.region_spinner) as Spinner
-        val nAdapter = ArrayAdapter.createFromResource(this, R.array.region_array, android.R.layout.simple_spinner_item)
+        val nAdapter = ArrayAdapter.createFromResource(this, R.array.region_arr, android.R.layout.simple_spinner_item)
         nAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.setAdapter(nAdapter)
+        region_spinner.adapter = nAdapter
     }
 
 
 
-    private var cnt: Int = 0
-
-    fun searchSummoner(view: View) {
+    fun searchSummoner(view: View)
+    {
         scroll_list.removeAllViews()
 
-        val region = region_spinner.getSelectedItem().toString();
-        val host = "https://${region}.api.riotgames.com"
-        val mode = mapOf("summoner" to "/lol/summoner/v4/summoners/by-name/",
-                            "league" to "/lol/league/v4/entries/by-summoner/",
-                            "match" to "/lol/match/v4/matchlists/by-account/"
-                        )
-        val api_key = "?api_key=RGAPI-9d877f40-5f36-4ce3-b3bb-f0830e54977d"
-
-        Toast.makeText(applicationContext, host, Toast.LENGTH_SHORT).show()
-
-
         val queue = Volley.newRequestQueue(this)
-        val url = host + mode["summoner"] + summonerName.text + api_key
 
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+        region = region_spinner.selectedItem.toString()
+        val url = proto + region + host + mode["summoner"] + summonerName.text + api_key
+
+        val jsonReq = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener {
-
                 val tmp = layoutInflater.inflate(R.layout.summoner_profile, null)
 
-                val imageRequest = ImageRequest("http://ddragon.leagueoflegends.com/cdn/9.3.1/img/profileicon/${it["profileIconId"]}.png",
+                val imgReq = ImageRequest("http://ddragon.leagueoflegends.com/cdn/9.3.1/img/profileicon/${it["profileIconId"]}.png",
                     Response.Listener<Bitmap> {
-
                         (tmp.findViewById(R.id.profile_icon) as ImageView).setImageBitmap(it)
-
                     }, 0, 0, Bitmap.Config.RGB_565, Response.ErrorListener {})
+                queue.add(imgReq)
 
-                queue.add(imageRequest)
+                id = it["id"].toString()
+                accountId = it["accountId"].toString()
+
+//                Toast.makeText(applicationContext, id, Toast.LENGTH_SHORT).show()
 
                 (tmp.findViewById(R.id.profile_name) as TextView).text = it["name"].toString()
                 (tmp.findViewById(R.id.profile_level) as TextView).text = it["summonerLevel"].toString()
@@ -65,7 +72,6 @@ class MainActivity : AppCompatActivity() {
                 scroll_list.addView(tmp)
             },
             Response.ErrorListener {
-
                 val tmp = TextView(this)
 
                 tmp.text = "Summoner Not Found!"
@@ -75,7 +81,6 @@ class MainActivity : AppCompatActivity() {
                 scroll_list.addView(tmp)
             }
         )
-
-        queue.add(jsonObjectRequest)
+        queue.add(jsonReq)
     }
 }
